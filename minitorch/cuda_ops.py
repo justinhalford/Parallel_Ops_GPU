1f7dad296996dc665ead9ccaaaac8e473ef661c8
justinhalford <justinhalford@gmail.com>
2022-11-12 20:25:47 -0500

    Initial commit

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -443,9 +443,7 @@ def _tensor_matrix_multiply(
     #    b) Copy into shared memory for b matrix
     #    c) Compute the dot produce for position c[i, j]
     # TODO: Implement for Task 3.4.
-    idx_x = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
-    idx_y = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
-    idx_z = cuda.blockIdx.z * cuda.blockDim.z + cuda.threadIdx.z
+    k = cuda.blockIdx.z * cuda.blockDim.z + cuda.threadIdx.z
 
     c_shared = cuda.shared.array((BLOCK_DIM, BLOCK_DIM), numba.float64)
     c_shared[pi][pj] = 0.0
@@ -456,14 +454,14 @@ def _tensor_matrix_multiply(
         # Block-(?, blockIdx.x, i) in a
         x_a = bX * BLOCK_DIM + pi
         y_a = i * BLOCK_DIM + pj
-        z_a = (idx_z if out_shape[0] == a_shape[0] else 0)
+        z_a = (k if out_shape[0] == a_shape[0] else 0)
         a_shared[pi][pj] = (a_storage[index_to_position((z_a, x_a, y_a), a_strides)] if x_a < a_shape[1] and y_a < a_shape[2] else 0.0)
         
 
         # Block (?, i, blockIdx.y) in b
         x_b = i * BLOCK_DIM + pi
         y_b = bY * BLOCK_DIM + pj
-        z_b = (idx_z if out_shape[0] == b_shape[0] else 0)
+        z_b = (k if out_shape[0] == b_shape[0] else 0)
         b_shared[pi][pj] = (b_storage[index_to_position((z_b, x_b, y_b), b_strides)] if x_b < b_shape[1] and y_b < b_shape[2] else 0.0)
 
         cuda.syncthreads()
@@ -473,8 +471,8 @@ def _tensor_matrix_multiply(
 
         cuda.syncthreads()
 
-    if idx_z < out_shape[0] and idx_x < out_shape[1] and idx_y < out_shape[2]:
-        pos = index_to_position((idx_z, idx_x, idx_y), out_strides)
+    if k < out_shape[0] and i < out_shape[1] and j < out_shape[2]:
+        pos = index_to_position((k, i, j), out_strides)
         out[pos] = c_shared[pi][pj]
     #raise NotImplementedError("Need to implement for Task 3.4")
 
