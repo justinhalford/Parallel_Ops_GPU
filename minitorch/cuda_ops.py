9541f4d6cd08c8f7cf909cf39952d2c68d2fb7dd
justinhalford <justinhalford@gmail.com>
2022-11-12 19:05:41 -0500

    Initial commit

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -358,28 +358,27 @@ def _mm_practice(out: Storage, a: Storage, b: Storage, size: int) -> None:
         size (int): size of the square
     """
     BLOCK_DIM = 32
+    
     # TODO: Implement for Task 3.3.
-    m1 = cuda.shared.array((BLOCK_DIM, BLOCK_DIM), numba.float64)
-    m2 = cuda.shared.array((BLOCK_DIM, BLOCK_DIM), numba.float64)
-    x = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
-    y = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
+    shm_a = cuda.shared.array((THREADS_PER_BLOCK, THREADS_PER_BLOCK), numba.float64)
+    shm_b = cuda.shared.array((THREADS_PER_BLOCK, THREADS_PER_BLOCK), numba.float64)
 
-    if x >= size or y >= size:
+    idx_x = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+    idx_y = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
+    if idx_x >= size or idx_y >= size:
         return
-        
-    pos = index_to_position((x, y), (size, 1))
-    m1[x][y] = a[pos]
-    m2[x][y] = b[pos]
+    
+    pos = index_to_position((idx_x, idx_y), (size, 1))
+    shm_a[idx_x][idx_y] = a[pos]
+    shm_b[idx_x][idx_y] = b[pos]
 
     cuda.syncthreads()
-    
-    
-    sum = 0.0
-    for i in range(size):
-        sum += m1[x][i] * m2[y][i]
-    out[pos] = sum
 
+    total = 0.0
+    for i in range(size):
+        total += shm_a[idx_x][i] * shm_b[i][idx_y]
     
+    out[pos] = total
     #raise NotImplementedError("Need to implement for Task 3.3")
 
 
